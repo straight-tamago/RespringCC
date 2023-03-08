1d155af0b9deca207d61a2dec57663ceadc2d537
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2023-03-08 19:51:48 +0900

    1.2

--- RespringCC/ContentView.swift
+++ RespringCC/ContentView.swift
@@ -8,43 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isURLSchemeOpened = false
+    @State var Show = 0
+    @State private var flag = true
     var body: some View {
-        BounceAnimationView(text: "Respring Now...", startTime: 0.0)
-        ActivityIndicator()
-            .onAppear {
-                CC()
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    respring()
+        VStack {
+            if Show == 1{
+                BounceAnimationView(text: "Respring Now...", startTime: 0.0)
+                ActivityIndicator()
+                    .onAppear {
+                        CC()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            if UserDefaults.standard.bool(forKey: "Respring_Mode") {
+                                respringFrontboard()
+                            }else{
+                                respringBackboard()
+                            }
+                        }
+                    }
+            }else if Show == 2{
+                NavigationView {
+                    List {
+                        VStack {
+                            Text("Respring Mode")
+                            Toggle(flag ? "Frontboard" : "Backboard", isOn: $flag)
+                                .onChange(of: flag) { newValue in
+                                    UserDefaults.standard.set(flag, forKey: "Respring_Mode")
+                                }
+                        }
+                    }
                 }
             }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                if isURLSchemeOpened {
+                    Show = 2
+                }else{
+                    Show = 1
+                }
+            }
+        }
+        .onOpenURL { url in
+            isURLSchemeOpened = true
+        }
     }
 }
 
 func CC() {
-    var Default_URL_STR = "/System/Library/ControlCenter/Bundles/MagnifierModule.bundle/Info.plist"
+    let Module_BasePath = "/System/Library/ControlCenter/Bundles/MagnifierModule.bundle"
+    var Default_URL_STR = "\(Module_BasePath)/Info.plist"
     guard var Import_URL = Bundle.main.url(forResource: "Default", withExtension: "plist") else { return }
     guard var Import_Data = try? Data(contentsOf: Import_URL) else { return }
-    guard var Import_Data = PlistPadding(Plist_Data: Import_Data, Default_URL_STR: Default_URL_STR) else { return }
+    guard var Import_Data = PlistPadding(Plist_Data: Import_Data, Target_URL: Default_URL_STR) else { return }
     print(OverwriteData(TargetFilePath: Default_URL_STR, OverwriteFileData: Import_Data))
     
-    Default_URL_STR = "/System/Library/ControlCenter/Bundles/MagnifierModule.bundle/Assets.car"
+    Default_URL_STR = "\(Module_BasePath)/Assets.car"
     guard var Import_URL = Bundle.main.url(forResource: "Default", withExtension: "car") else { return }
     guard var Import_Data = try? Data(contentsOf: Import_URL) else { return }
     print(OverwriteData(TargetFilePath: Default_URL_STR, OverwriteFileData: Import_Data))
+    
+    do {
+        let files = try FileManager.default.contentsOfDirectory(atPath: Module_BasePath)
+        files.forEach { file in
+            if file.contains("lproj") {
+                let lproj = "\(Module_BasePath)/\(file)/InfoPlist.strings"
+                print(OverwriteData_XXX(TargetFilePath: lproj, OverwriteData: "xxx"))
+            }
+        }
+    } catch {
+        print(error.localizedDescription)
+    }
 }
 
-func PlistPadding(Plist_Data: Data, Default_URL_STR: String) -> Data? {
-    guard let Default_Data = try? Data(contentsOf: URL(fileURLWithPath: Default_URL_STR)) else { return nil }
-    if Plist_Data.count == Default_Data.count { return Plist_Data }
+func PlistPadding(Plist_Data: Data, Target_URL: String) -> Data? {
+    guard let Target_Data = try? Data(contentsOf: URL(fileURLWithPath: Target_URL)) else { return nil }
+    if Plist_Data.count == Target_Data.count { return Plist_Data }
     guard var Plist = try? PropertyListSerialization.propertyList(from: Plist_Data, format: nil) as? [String:Any] else { return nil }
     var EditedDict = Plist as! [String: Any]
     EditedDict.updateValue(Bundle.main.bundleIdentifier, forKey: "CCLaunchApplicationIdentifier")
     guard var newData = try? PropertyListSerialization.data(fromPropertyList: EditedDict, format: .binary, options: 0) else { return nil }
     var count = 0
-    print("DefaultData - "+String(Default_Data.count))
+    print("DefaultData - "+String(Target_Data.count))
     while true {
         newData = try! PropertyListSerialization.data(fromPropertyList: EditedDict, format: .binary, options: 0)
-        if newData.count >= Default_Data.count { break }
+        if newData.count >= Target_Data.count { break }
         count += 1
         EditedDict.updateValue(String(repeating:"0", count:count), forKey: "0")
     }
--- RespringCC/Overwrite.swift
+++ RespringCC/Overwrite.swift
@@ -44,3 +44,35 @@ func OverwriteData(TargetFilePath: String, OverwriteFileData: Data) -> String {
     }
     return "Success"
 }
+
+func OverwriteData_XXX(TargetFilePath: String, OverwriteData: String) -> String {
+    let OverwriteFileData = OverwriteData.data(using: .utf8)!
+    let fd = open(TargetFilePath, O_RDONLY | O_CLOEXEC)
+    defer { close(fd) }
+    let Map = mmap(nil, OverwriteFileData.count, PROT_READ, MAP_SHARED, fd, 0)
+    if Map == MAP_FAILED {
+        return "mmap Error"
+    }
+    guard mlock(Map, OverwriteFileData.count) == 0 else {
+        return "mlock Error"
+    }
+    for chunkOff in stride(from: 0, to: OverwriteFileData.count, by: 0x4000) {
+        let dataChunk = OverwriteFileData[chunkOff..<min(OverwriteFileData.count, chunkOff + 0x3fff)]
+        var overwroteOne = false
+        for _ in 0..<2 {
+            let overwriteSucceeded = dataChunk.withUnsafeBytes { dataChunkBytes in
+                return unaligned_copy_switch_race(
+                    fd, Int64(chunkOff), dataChunkBytes.baseAddress, dataChunkBytes.count)
+            }
+            if overwriteSucceeded {
+                overwroteOne = true
+                break
+            }
+            sleep(1)
+        }
+        guard overwroteOne else {
+            return "unknown Error"
+        }
+    }
+    return "Success"
+}
